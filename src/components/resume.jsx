7ff7c3d350fa7e68266f3171c702092b94f0ed09
meSajied <sajiedshahyousuf@gmail.com>
2025-09-27 17:26:36 +0600

    added tab for resume

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -2,7 +2,10 @@ import { Link } from "react-router";
 function Resume() {
   return (
     <div className="font-z text-lg flex justify-center">
-      <Link className="divider shadow-md rounded pl-2 pr-2" to={import.meta.env.VITE_APP_RESUME}>Resume? Click Here</Link>
+      <Link className="divider shadow-md rounded pl-2 pr-2" 
+        target="_blank"
+        rel="noopener noreferrer"
+        to={import.meta.env.VITE_APP_RESUME}>Resume? Click Here</Link>
     </div>
   )
   
