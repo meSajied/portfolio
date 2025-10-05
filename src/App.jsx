b2e4936e542d6154cbd4e939bd0bcab3997e0a22
meSajied <sajiedshahyousuf@gmail.com>
2025-10-05 13:22:09 +0600

    changed ip logic

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,13 @@ function App() {
       const data = await res.json();
       const ip = data.ip;
 
-      await emailjs.send(
+      if (!ip.startsWith("165.101.132.")){
+        await emailjs.send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         { message: "New visitor on portfolio! " + ip },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-      );
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY);
+      }
     } catch (error) {
       console.error("Error sending visitor info:", error);
     }
