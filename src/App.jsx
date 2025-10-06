a5466ac22ff0d8892c7dff5de6ad27ed12aadc64
meSajied <sajiedshahyousuf@gmail.com>
2025-10-06 15:33:12 +0600

    set range for ip

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,9 @@ function App() {
       const data = await res.json();
       const ip = data.ip;
 
-      if (!ip.startsWith("165.101.132.")){
+      const regex = /^165\.101\.132\.(209|21[0-9]|22[0-2])$/;
+
+      if(!regex.test(ip)) {
         await emailjs.send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
