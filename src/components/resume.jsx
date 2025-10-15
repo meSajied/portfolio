a64c27cc99a10189105a017e97f17319b047d47b
meSajied <sajiedshahyousuf@gmail.com>
2025-10-15 13:35:46 +0600

    added notes again

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 function Resume() {
   return (
     <div className="font-z text-base flex justify-center">
-      <p>Wanna know about my learnings during pursuing degree?</p>
+      <p>Wanna know what I learned while pursuing my degree?</p>
       <Link className="pl-2 pr-2 italic underline" 
         target="_blank"
         rel="noopener noreferrer"
